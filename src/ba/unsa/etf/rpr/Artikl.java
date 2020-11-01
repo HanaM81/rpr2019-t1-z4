7d7cc4c06801c21d116a7305c374f234ca700abb
Hana Mušović <hmusovic2@etf.unsa.ba>
2020-11-01 14:26:51 +0100

    Dodane ostale klase.

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,12 +1,23 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
-    public Object getNaziv() {
+    private String naziv;
+    private int cijena;
+    private String kod;
+    public Artikl(String naziv, int cijena, String kod) {
+        this.naziv=naziv;
+        this.cijena=cijena;
+        this.kod=kod;
+    }
+    public String getNaziv() {
+        return naziv;
+    }
+    public int getCijena() {
+        return cijena;
+    }
+    public String getKod() {
+        return kod;
     }
 
-    public Object getCijena() {
-    }
 
-    public Object getKod() {
-    }
 }
